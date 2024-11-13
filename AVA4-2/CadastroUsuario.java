6757bef5f255cfd3807b637ac08873bc220476d6
pedro-aurelio-10 <pedroaurelio313@gmail.com>
2024-11-13 10:26:52 -0300

    AVA4-2

---
+++ AVA4-2/CadastroUsuario.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class CadastroUsuario {
+
+    public CadastroUsuario(JFrame principal) {
+        JDialog cadastroUsuarios = new JDialog(principal, "Cadastro de Usuários", true);
+        cadastroUsuarios.setSize(600, 300);
+        cadastroUsuarios.setLayout(new BorderLayout());
+
+        JLabel titulo = new JLabel("Cadastro de Usuários", SwingConstants.CENTER);
+        cadastroUsuarios.add(titulo, BorderLayout.NORTH);
+
+        JPanel painelCampos = new JPanel(new GridLayout(4, 2, 5, 5));
+        painelCampos.add(new JLabel("Usuário:"));
+        painelCampos.add(new JTextField(25));
+        painelCampos.add(new JLabel("Senha:"));
+        painelCampos.add(new JPasswordField(15));
+        painelCampos.add(new JLabel("Email:"));
+        painelCampos.add(new JTextField(30));
+        painelCampos.add(new JLabel("Ativo:"));
+        JRadioButton radioAtivo = new JRadioButton();
+        painelCampos.add(radioAtivo);
+
+        JPanel painelBotoes = BotoesCrud.criarPainelBotoes(cadastroUsuarios);
+
+        cadastroUsuarios.add(painelCampos, BorderLayout.CENTER);
+        cadastroUsuarios.add(painelBotoes, BorderLayout.SOUTH);
+        cadastroUsuarios.setLocationRelativeTo(principal);
+        cadastroUsuarios.setVisible(true);
+    }
+}
