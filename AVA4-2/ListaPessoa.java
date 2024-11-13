bc705c9f293077b3d9d9b694e567e1e284bf6aa7
pedro-aurelio-10 <pedroaurelio313@gmail.com>
2024-11-13 10:38:23 -0300

    Avaliação 4 Parcial - GUI Cadastros

---
+++ AVA4-2/ListaPessoa.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class ListaPessoa {
+
+    public ListaPessoa(JFrame principal) {
+        JDialog listaPessoas = new JDialog(principal, "Lista de Pessoas", true);
+        listaPessoas.setSize(750, 650);
+        listaPessoas.setLayout(new BorderLayout());
+
+        listaPessoas.add(new JLabel("Lista de Pessoas", SwingConstants.CENTER), BorderLayout.NORTH);
+        listaPessoas.add(new JTextArea("Exemplo de lista de pessoas..."), BorderLayout.CENTER);
+
+        JButton btnFechar = new JButton("Fechar");
+        btnFechar.addActionListener(e -> listaPessoas.setVisible(false));
+        listaPessoas.add(btnFechar, BorderLayout.SOUTH);
+
+        listaPessoas.setLocationRelativeTo(principal);
+        listaPessoas.setVisible(true);
+    }
+}
